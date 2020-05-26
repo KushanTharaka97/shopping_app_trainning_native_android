8dfb9cd099e96414d8de7d196376c4bd9a802555
tharaka <tharakakushan@gmail.com>
2020-05-26 10:51:14 +0530

    styling UI layout:New with Linearlayout UI designning:Add String Methods to display

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -26,8 +26,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity*5);
+        String priceMessage=(quantity*10) + " for " + quantity + " cups of coffie.Pay Up! " ;
+        displayMessage(priceMessage);
+
     }
 
     public void increment(View view){
@@ -59,5 +60,12 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
